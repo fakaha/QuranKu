de29c6d2ba638aa76f77b3130b6f5fcf7d31c9f2
fakaha <lunastra34@gmail.com>
2024-08-08 21:31:56 +0700

    style: change list surah from flex to grid display

--- src/components/ListSurah.jsx
+++ src/components/ListSurah.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export const ListSurah = (props) => {
   return (
-    <div className="flex w-[26%] items-center justify-between">
+    <div className="flex items-center justify-between">
       <div className="flex justify-center items-center gap-2">
         <p className="flex text-[#fff] justify-center items-center bg-[#87D1A4] w-[36px] h-[36px] rounded-lg">
           {props.dataParent.number}
         </p>
         <div className="flex flex-col">
           <p>{props.dataParent.englishName}</p>
-          <p className="">{props.dataParent.englishNameTranslation}</p>
+          <div className="flex text-xs">
+            <p>{props.dataParent.englishNameTranslation}&nbsp;</p>
+            <p>({props.dataParent.numberOfAyahs})</p>
+          </div>
         </div>
       </div>
       <p className="text-[#076C58] font-bold">{props.dataParent.name}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { fetchSurah } from '../services/SurahServices'
-import { ListSurah } from '../components/ListSurah'
+import React, { useEffect, useState } from "react";
+import { fetchSurah } from "../services/SurahServices";
+import { ListSurah } from "../components/ListSurah";
 
 export const Home = () => {
-    const [Surah, setSurah] = useState([])
-    useEffect(() => {
-        fetchSurah((data) => {
-            setSurah(data)
-        });
-    }, [])
+  const [Surah, setSurah] = useState([]);
+  useEffect(() => {
+    fetchSurah((data) => {
+      setSurah(data);
+    });
+  }, []);
   return (
-    <div className='p-8'>
-      <h1 className='text-center text-2xl font-bold text-[#004B40] my-3'>QuranKu</h1>
-      <div className='flex flex-wrap gap-6 justify-center'>
+    <div className="py-8 px-12">
+      <h1 className="text-center text-2xl font-bold text-[#004B40] my-3">
+        QuranKu
+      </h1>
+      <input className="border-[#333] border-2 rounded-lg px-2 py-1" type="text" placeholder="Search Surah" />
+
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3">
         {Surah.map((result) => (
-            <ListSurah key={result.number} dataParent={result}/>
+          <ListSurah key={result.number} dataParent={result} />
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
